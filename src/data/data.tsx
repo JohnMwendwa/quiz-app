5e6c07035ef606cd8a51066cf83ee6f8d66dfb0d
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-30 07:01:34 +0300

    add questions,infer types and export data

---
+++ src/data/data.tsx
@@ -0,0 +1,73 @@
+import { Question } from "../models/question";
+
+const questions: Question[] = [
+  {
+    question: "What does CSS stand for?",
+    answers: [
+      "Computer Style Sheets",
+      "Creative Style Sheets",
+      "Cascading Style Sheets",
+      "Colorful Style Sheets",
+    ],
+    correct: 3,
+  },
+
+  {
+    question:
+      "Where in an HTML document is the correct place to refer to an external style sheet?",
+    answers: [
+      "In the <head> section",
+      "In the <body> section",
+      "At the end of the document",
+      "You can't refer to an external style sheet",
+    ],
+    correct: 1,
+  },
+  {
+    question: "Which HTML tag is used to define an internal style sheet?",
+    answers: ["<style>", "<script>", "<headStyle>", "<css>"],
+    correct: 1,
+  },
+  {
+    question: "Which HTML attribute is used to define inline styles?",
+    answers: ["class", "style", "font", "styles"],
+    correct: 2,
+  },
+  {
+    question: "Which is the correct CSS syntax?",
+    answers: [
+      "{body:color=black;}",
+      "{body;color:black;}",
+      "body:color=black;",
+      "body {color: black;}",
+    ],
+    correct: 4,
+  },
+  {
+    question: "How do you insert a comment in a CSS file?",
+    answers: [
+      "' this is a comment",
+      "/* this is a comment */",
+      "// this is a comment",
+      "// this is a comment //",
+    ],
+    correct: 2,
+  },
+  {
+    question: "Which property is used to change the background color?",
+    answers: ["color", "bgcolor", "background-color", "bgColor"],
+    correct: 3,
+  },
+  {
+    question: "How do you add a background color for all <h1> elements?",
+    answers: [
+      "all.h1 {background-color:#FFFFFF;}",
+      "h1.setAll {background-color:#FFFFFF;}",
+      "h1.all {background-color:#FFFFFF;}",
+      "h1 {background-color:#FFFFFF;}",
+    ],
+    correct: 4,
+  },
+];
+
+export default questions;
