a659ef4767569866e6b46167ff2e2d3789c39bb1
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-30 17:07:47 +0300

    loop and display question and available choices

--- src/components/QuizCard.tsx
+++ src/components/QuizCard.tsx
@@ -1,5 +1,55 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
+import { useQuizContext } from "../context/QuizContext";
+import { Question } from "../models/question";
+
+interface Answer {
+  no: number;
+  answer: number;
+  isCorrect: boolean;
+}
 
 export default function QuizCard() {
-  return <div>QuizCard</div>;
+  const {
+    questions,
+    currentQuestion,
+    back,
+    next,
+    isFirstQuestion,
+    isLastQuestion,
+  } = useQuizContext();
+
+  const [question, setQuestion] = useState<Question>();
+
+  useEffect(() => {
+    setQuestion(questions[currentQuestion]);
+  }, [question, currentQuestion]);
+
+  const handleSubmit = () => {
+    if (!isLastQuestion) return next();
+
+    alert("Submitted!");
+  };
+
+  return (
+    <>
+      <h1>Quiz Card</h1>
+
+      <h3>{question?.question}</h3>
+
+      {question?.answers.map((answer, idx) => {
+        return (
+          <div key={answer}>
+            {idx + 1}. {answer}
+          </div>
+        );
+      })}
+
+      {!isFirstQuestion && <button onClick={back}>Back</button>}
+
+      <button onClick={handleSubmit}>
+        {isLastQuestion ? "Finish" : "Next"}
+      </button>
+    </>
+  );
 }
