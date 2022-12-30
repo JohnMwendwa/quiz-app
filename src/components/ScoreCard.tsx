2ca46f412300bc63bf26d25d175ac98fbb5a9918
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-30 23:48:53 +0300

    create function for giving the final score review/remarks

--- src/components/ScoreCard.tsx
+++ src/components/ScoreCard.tsx
@@ -15,19 +15,42 @@ export default function ScoreCard({
   const { questions } = useQuizContext();
 
   // count the number of correct answers a user has
-  const correctQuestions = answers.reduce((current, answer) => {
+  const correctQuestions: number = answers.reduce((current, answer) => {
     if (answer.isCorrect) {
       return current + 1;
     }
     return current;
   }, 0);
 
+  let scoreReview: string;
+
+  switch (correctQuestions) {
+    case 0:
+    case 1:
+    case 2:
+      scoreReview = "Poor 😭";
+      break;
+    case 3:
+    case 4:
+      scoreReview = "Average 🙂";
+      break;
+    case 5:
+    case 6:
+      scoreReview = "Good 😊";
+      break;
+    case 7:
+      scoreReview = "Excellent 🤩";
+      break;
+    default:
+      scoreReview = "Genius";
+  }
+
   return (
     <>
       <h1>Your Score</h1>
 
       <div>{`${correctQuestions}/${questions.length}`}</div>
-
+      <div>Score Review : {scoreReview}</div>
       <button onClick={handleReview}>Review</button>
       <button onClick={handleRestart}>Restart</button>
     </>
