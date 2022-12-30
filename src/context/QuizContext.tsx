141eba3a9a55e038aac5a076d25fb3e262ebc516
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-30 07:30:45 +0300

    create a function for consuming the quiz context

--- src/context/QuizContext.tsx
+++ src/context/QuizContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 import questions from "../data/data";
 import { Question } from "../models/question";
@@ -19,4 +19,8 @@ export const QuizContextProvider = ({ children }: Props) => {
       {children}
     </QuizContext.Provider>
   );
+};
+
+export const useQuizContext = () => {
+  return useContext(QuizContext);
 };
