ce1b9cc1a6a0760eea0d50dd8693fed4cd46c4b5
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-30 08:03:20 +0300

    create a function for going back

--- src/context/QuizContext.tsx
+++ src/context/QuizContext.tsx
@@ -1,10 +1,11 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 import questions from "../data/data";
 import { Question } from "../models/question";
 
 interface ContextProps {
   questions: Question[];
+  currentQuestion: Number;
 }
 
 interface Props {
@@ -14,8 +15,17 @@ interface Props {
 const QuizContext = createContext({} as ContextProps);
 
 export const QuizContextProvider = ({ children }: Props) => {
+  const [isCorrect, setiIsCorrect] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const back = () => {
+    if (currentQuestion !== 0) {
+      return setCurrentQuestion((prev) => prev - 1);
+    }
+    return;
+  };
   return (
-    <QuizContext.Provider value={{ questions }}>
+    <QuizContext.Provider value={{ questions, currentQuestion }}>
       {children}
     </QuizContext.Provider>
   );
