352e22498c1332323e3d2191d467db55a8563100
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-30 19:00:07 +0300

    create a button fro starting session

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -1,3 +1,12 @@
-export default function Instructions() {
-  return <div>Instructions</div>;
+interface InstructionsProps {
+  startSession: (value: boolean) => void;
+}
+
+export default function Instructions({ startSession }: InstructionsProps) {
+  return (
+    <div>
+      <h1>Instructions</h1>
+      <button onClick={() => startSession(true)}>Start</button>
+    </div>
+  );
 }
