359fb4c9f71843b70e7e31fbee1e956c6f6c4278
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-30 18:35:38 +0300

    define QuizCard props interface

--- src/components/QuizCard.tsx
+++ src/components/QuizCard.tsx
@@ -1,59 +1,20 @@
-import { useState, useEffect } from "react";
-
 import { useQuizContext } from "../context/QuizContext";
 import { Question } from "../models/question";
 
-interface Answer {
-  no: number;
-  answer: number;
-  isCorrect: boolean;
+interface QuizCardProps {
+  question?: Question;
+  currentQuestion: number;
+  updateAnswers: (currentQuestion: number, idx: number) => void;
+  handleSubmit: () => void;
 }
 
-export default function QuizCard() {
-  const {
-    questions,
-    currentQuestion,
-    back,
-    next,
-    isFirstQuestion,
-    isLastQuestion,
-  } = useQuizContext();
-
-  const [question, setQuestion] = useState<Question>();
-  const [UserAnswer, setUserAnswer] = useState<Answer[]>([]);
-
-  useEffect(() => {
-    setQuestion(questions[currentQuestion]);
-  }, [question, currentQuestion]);
-
-  const updateAnswers = (no: number, answer: number) => {
-    const idx = UserAnswer?.findIndex((answer) => answer.no === no);
-
-    const answerObj: Answer = {
-      no,
-      answer,
-      isCorrect: answer + 1 === question?.correct,
-    };
-
-    if (idx === -1) {
-      return setUserAnswer((prev) => [...prev, answerObj]);
-    }
-
-    setUserAnswer((prev) =>
-      prev?.map((obj) => {
-        if (obj.no === no) {
-          return answerObj;
-        }
-        return obj;
-      })
-    );
-  };
-
-  const handleSubmit = () => {
-    if (!isLastQuestion) return next();
-
-    alert("Submitted!");
-  };
+export default function QuizCard({
+  question,
+  currentQuestion,
+  updateAnswers,
+  handleSubmit,
+}: QuizCardProps) {
+  const { back, isFirstQuestion, isLastQuestion } = useQuizContext();
 
   return (
     <>
