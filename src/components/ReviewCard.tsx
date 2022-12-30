b4e82f572f06bf987a6931c5976cc52ec345be68
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-30 22:11:33 +0300

    infer types on ReviewCard props

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -1,3 +1,38 @@
-export default function ReviewCard() {
-  return <div>ReviewCard</div>;
+import { Question } from "../models/question";
+import { Answer } from "../models/answer";
+import { useQuizContext } from "../context/QuizContext";
+
+interface ReviewCardProps {
+  question: Question;
+  userAnswer: Answer[];
+  handleFinishReview: () => void;
+}
+
+export default function ReviewCard({
+  question,
+  handleFinishReview,
+  userAnswer,
+}: ReviewCardProps) {
+  const { isLastQuestion, isFirstQuestion, back } = useQuizContext();
+  return (
+    <>
+      <h1>Quiz Card</h1>
+
+      <h3>{question?.question}</h3>
+
+      {question?.answers.map((answer, idx) => {
+        return (
+          <div key={answer}>
+            {idx + 1}. {answer}
+          </div>
+        );
+      })}
+
+      {!isFirstQuestion && <button onClick={back}>Back</button>}
+
+      <button onClick={handleFinishReview}>
+        {isLastQuestion ? "Finish Review" : "Next"}
+      </button>
+    </>
+  );
 }
