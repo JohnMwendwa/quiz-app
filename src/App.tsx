0d2ba873ec8d933371e68ff3089f0909b8d47b7f
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-30 18:34:46 +0300

    move state upwards

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,63 @@
+import { useState, useEffect } from "react";
+
+import { useQuizContext } from "./context/QuizContext";
+import { Question } from "./models/question";
 import QuizCard from "./components/QuizCard";
 
+interface Answer {
+  no: number;
+  answer: number;
+  isCorrect: boolean;
+}
+
 function App() {
+  const { questions, currentQuestion, isLastQuestion, next } = useQuizContext();
+  const [UserAnswer, setUserAnswer] = useState<Answer[]>([]);
+  const [question, setQuestion] = useState<Question>();
+
+  useEffect(() => {
+    setQuestion(questions[currentQuestion]);
+  }, [question, currentQuestion]);
+
+  const updateAnswers = (no: number, answer: number) => {
+    const idx = UserAnswer?.findIndex((answer) => answer.no === no);
+
+    const answerObj: Answer = {
+      no,
+      answer,
+      isCorrect: answer + 1 === question?.correct,
+    };
+
+    // only update user asnswers if it's a new one
+    if (idx === -1) {
+      return setUserAnswer((prev) => [...prev, answerObj]);
+    }
+
+    // update an existing answer
+    setUserAnswer((prev) =>
+      prev?.map((obj) => {
+        if (obj.no === no) {
+          return answerObj;
+        }
+        return obj;
+      })
+    );
+  };
+
+  const handleSubmit = () => {
+    if (!isLastQuestion) return next();
+
+    alert("Submitted!");
+  };
+
   return (
     <>
-      <QuizCard />
+      <QuizCard
+        question={question}
+        currentQuestion={currentQuestion}
+        updateAnswers={updateAnswers}
+        handleSubmit={handleSubmit}
+      />
     </>
   );
 }
