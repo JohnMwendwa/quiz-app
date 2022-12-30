241b38ab4680bc9584fcf406f88de100c591508a
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-30 07:28:07 +0300

    wrap the App component with the Quiz context provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { QuizContextProvider } from "./context/QuizContext";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QuizContextProvider>
+      <App />
+    </QuizContextProvider>
   </React.StrictMode>
 );
