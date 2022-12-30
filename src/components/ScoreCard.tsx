6bb154de0e6f5d43237d0e8b3b47e171fb88e32d
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-30 22:39:18 +0300

    create ScoreCard props interface

--- src/components/ScoreCard.tsx
+++ src/components/ScoreCard.tsx
@@ -1,3 +1,17 @@
-export default function ScoreCard() {
-  return <div>ScoreCard</div>;
+interface ScoreCardProps {
+  handleRestart: () => void;
+  handleReview: () => void;
+}
+
+export default function ScoreCard({
+  handleRestart,
+  handleReview,
+}: ScoreCardProps) {
+  return (
+    <>
+      <h1>Your Score</h1>
+      <button onClick={handleReview}>Review</button>
+      <button onClick={handleRestart}>Restart</button>
+    </>
+  );
 }
