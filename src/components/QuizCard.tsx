bd764f9bba61233ac0df5a450e1beeaa0afb3dcc
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-31 13:47:36 +0300

    style the QuizCard component

--- src/components/QuizCard.tsx
+++ src/components/QuizCard.tsx
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+
 import { useQuizContext } from "../context/QuizContext";
 import { Question } from "../models/question";
 
@@ -8,31 +10,79 @@ interface QuizCardProps {
   handleSubmit: () => void;
 }
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 400px;
+  position: relative;
+
+  h1 {
+    text-align: center;
+    margin-bottom: 10px;
+  }
+
+  button {
+    padding: 5px 20px;
+    margin-top: 10px;
+    cursor: pointer;
+  }
+`;
+
+const Option = styled.div`
+  border: 1px solid black;
+  padding: 5px 10px;
+  border-radius: 3px;
+  margin-top: 10px;
+  cursor: pointer;
+`;
+
+const Counter = styled.div`
+  position: absolute;
+  top: -30px;
+  right: -30px;
+  background-color: black;
+  color: white;
+  border: 1px solid black;
+  border-radius: 0 3px 0 20px;
+  padding: 3px 15px;
+  font-weight: 900;
+`;
+
 export default function QuizCard({
   question,
   currentQuestion,
   updateAnswers,
   handleSubmit,
 }: QuizCardProps) {
-  const { isLastQuestion } = useQuizContext();
+  const { questions, isLastQuestion } = useQuizContext();
 
   return (
-    <>
+    <Container>
+      <Counter>
+        {currentQuestion + 1} / {questions.length}
+      </Counter>
+
       <h1>Quiz Card</h1>
 
-      <h3>{question?.question}</h3>
+      <h2>{question?.question}</h2>
 
-      {question?.answers.map((answer, idx) => {
-        return (
-          <div key={answer} onClick={() => updateAnswers(currentQuestion, idx)}>
-            {idx + 1}. {answer}
-          </div>
-        );
-      })}
+      <div>
+        {question?.answers.map((answer, idx) => {
+          return (
+            <Option
+              key={answer}
+              onClick={() => updateAnswers(currentQuestion, idx)}
+            >
+              {idx + 1}. {answer}
+            </Option>
+          );
+        })}
+      </div>
 
       <button onClick={handleSubmit}>
         {isLastQuestion ? "Finish" : "Next"}
       </button>
-    </>
+    </Container>
   );
 }
