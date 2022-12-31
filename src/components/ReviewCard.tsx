f6786071e334de970569f213b14789b16b82dd90
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-31 14:44:57 +0300

    style the ReviewCard component

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -1,6 +1,9 @@
+import styled from "styled-components";
+
 import { Question } from "../models/question";
 import { Answer } from "../models/answer";
 import { useQuizContext } from "../context/QuizContext";
+import { Container, Option, Counter } from "./QuizCard";
 
 interface ReviewCardProps {
   question?: Question;
@@ -8,31 +11,62 @@ interface ReviewCardProps {
   handleFinishReview: () => void;
 }
 
+const Wrapper = styled(Option)`
+  &.correct {
+    border: none;
+    outline: 2px solid green;
+  }
+`;
+
+const Buttons = styled.div`
+  button {
+    margin-right: 10px;
+
+    :last-child {
+      background-color: black;
+      color: white;
+    }
+  }
+`;
+
 export default function ReviewCard({
   question,
   handleFinishReview,
   userAnswer,
 }: ReviewCardProps) {
-  const { isLastQuestion, isFirstQuestion, back } = useQuizContext();
+  const { isLastQuestion, isFirstQuestion, back, questions, currentQuestion } =
+    useQuizContext();
+
   return (
-    <>
+    <Container>
+      <Counter>
+        {currentQuestion + 1} / {questions.length}
+      </Counter>
+
       <h1>Review Card</h1>
 
       <h3>{question?.question}</h3>
 
-      {question?.answers.map((answer, idx) => {
-        return (
-          <div key={answer}>
-            {idx + 1}. {answer}
-          </div>
-        );
-      })}
+      <div>
+        {question?.answers.map((answer, idx) => {
+          return (
+            <Wrapper
+              key={answer}
+              className={idx + 1 === question.correct ? "correct" : ""}
+            >
+              {idx + 1}. {answer}
+            </Wrapper>
+          );
+        })}
+      </div>
 
-      {!isFirstQuestion && <button onClick={back}>Back</button>}
+      <Buttons>
+        {!isFirstQuestion && <button onClick={back}>Back</button>}
 
-      <button onClick={handleFinishReview}>
-        {isLastQuestion ? "Finish Review" : "Next"}
-      </button>
-    </>
+        <button onClick={handleFinishReview}>
+          {isLastQuestion ? "Finish Review" : "Next"}
+        </button>
+      </Buttons>
+    </Container>
   );
 }
